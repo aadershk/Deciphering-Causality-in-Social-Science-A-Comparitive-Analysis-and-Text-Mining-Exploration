8ee2235f60ca41d0b64b4914a003e07f92c5380a
aadershk <122157112+aadershk@users.noreply.github.com>
2024-10-15 23:17:10 +0200

    Create sentence_splitter.py

---
+++ sentence_splitter.py
@@ -0,0 +1,7 @@
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+def split_into_sentences(text):
+    doc = nlp(text)
+    return [sent.text.strip() for sent in doc.sents if len(sent.text.strip()) > 0]
